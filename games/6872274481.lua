82874caad1a4517a826197af24e2a3ef9e5c4f8e
Sortex [ SFX ] <130068806+LorisHost@users.noreply.github.com>
2025-04-19 20:49:59 +0300

    Improved Auto Balloon.

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -6033,41 +6033,59 @@ run(function()
 end)
 	
 run(function()
-	local AutoBalloon
-	
-	AutoBalloon = vape.Categories.Utility:CreateModule({
-		Name = 'AutoBalloon',
-		Function = function(callback)
-			if callback then
-				repeat task.wait() until store.matchState ~= 0 or (not AutoBalloon.Enabled)
-				if not AutoBalloon.Enabled then return end
-	
-				local lowestpoint = math.huge
-				for _, v in store.blocks do
-					local point = (v.Position.Y - (v.Size.Y / 2)) - 50
-					if point < lowestpoint then 
-						lowestpoint = point 
-					end
-				end
-	
-				repeat
-					if entitylib.isAlive then
-						if entitylib.character.RootPart.Position.Y < lowestpoint and (lplr.Character:GetAttribute('InflatedBalloons') or 0) < 3 then
-							local balloon = getItem('balloon')
-							if balloon then
-								for _ = 1, 3 do 
-									bedwars.BalloonController:inflateBalloon() 
-								end
-							end
-							task.wait(0.1)
-						end
-					end
-					task.wait(0.1)
-				until not AutoBalloon.Enabled
-			end
-		end,
-		Tooltip = 'Inflates when you fall into the void'
-	})
+    local AutoBalloon
+    
+    AutoBalloon = vape.Categories.Utility:CreateModule({
+        Name = 'AutoBalloon',
+        Function = function(callback)
+            if callback then
+                repeat 
+                    task.wait(0.5)
+                until store.matchState ~= 0 or (not AutoBalloon.Enabled)
+                
+                if not AutoBalloon.Enabled then return end
+                
+                local character = entitylib.character
+                local rootPart = character and character.RootPart
+                local balloonController = bedwars.BalloonController
+                
+                if not (character and rootPart and balloonController) then
+                    AutoBalloon.Toggle(false)
+                    return
+                end
+                
+                local lowestpoint = math.huge
+                for _, v in pairs(store.blocks) do
+                    if v and v.Position and v.Size then
+                        local point = (v.Position.Y - (v.Size.Y / 2)) - 50
+                        lowestpoint = math.min(lowestpoint, point)
+                    end
+                end
+                
+                while AutoBalloon.Enabled and entitylib.isAlive do
+                    if rootPart.Position.Y < lowestpoint then
+                        local currentBalloons = lplr.Character:GetAttribute('InflatedBalloons') or 0
+                        
+                        if currentBalloons < 3 then
+                            local balloon = getItem('balloon')
+                            if balloon then
+                                for i = currentBalloons + 1, 3 do
+                                    if not AutoBalloon.Enabled then break end
+                                    balloonController:inflateBalloon()
+                                    task.wait(0.1)
+                                end
+                            end
+                        end
+                    end
+                    
+                    local waitTime = rootPart.Position.Y < (lowestpoint + 10) and 0.1 or 0.3
+                    task.wait(waitTime)
+                end
+            end
+        end,
+        Tooltip = 'Automatically inflates balloons when falling into void',
+        Priority = 10
+    })
 end)
 	
 run(function()
