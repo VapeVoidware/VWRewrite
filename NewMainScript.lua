b3a0e34adc2195ad03491ebbb15724f47fd93c46
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-11-26 22:28:57 +0200

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -143,12 +143,12 @@ if (not CheatEngineMode) then checkDebug() end
 if shared.ForceDisableCE then CheatEngineMode = false; shared.CheatEngineMode = false end
 shared.CheatEngineMode = shared.CheatEngineMode or CheatEngineMode
 local s1, vv = pcall(function() return string.find(string.lower(tostring(identifyexecutor())), 'wave') end)
-for i = 1, 100 do
-    warn(`RESULT FUCK YOU WAVE {tostring(s1)} | {tostring(vv)}`)
-end
 if s1 and vv then
     shared.CheatEngineMode = false
     CheatEngineMode = false
+end
+for i = 1, 100 do
+    warn(`FINAL FINAL RESULT: {tostring(shared.CheatEngineMode)}`)
 end
 if (not isfolder('vape')) then makefolder('vape') end
 if (not isfolder('rise')) then makefolder('rise') end
