60f362aefcca10462b49ce2de6ad4fd937261f30
Erchobg <67239055+Erchobg@users.noreply.github.com>
2026-02-04 16:28:29 +0200

    Fixes
    MouseTP
    DaveyTP
    PearlTP
    Desync
    FastBow
    Reach

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -837,6 +837,11 @@ L=setmetatable({
 SharedConstants=safeRequire("SharedConstants",function()
 return require(n.TS['shared-constants'])
 end),
+CooldownController=safeRequire("CooldownController",function()
+return ac.resolveDependency
+"@easy-games/game-core:client/controllers/cooldown/cooldown-controller@CooldownController"
+
+end),
 AbilityController=safeRequire("AbilityController",function()
 return ac.resolveDependency
 "@easy-games/game-core:client/controllers/ability/ability-controller@AbilityController"
