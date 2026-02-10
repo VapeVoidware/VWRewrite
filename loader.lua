895d900fc7ff25b953c57afefbf4fa2c8cfbfae7
Erchobg <67239055+Erchobg@users.noreply.github.com>
2026-02-10 16:41:28 +0200

    fixes

--- loader.lua
+++ loader.lua
@@ -236,7 +236,7 @@ end
 
 --if not shared.VapeDeveloper then
 local TESTING_COMMIT = "master"
-local PRODUCTION_COMMIT = "f3d3ed957523f6396a7e5170ff239ffaab9677d6"
+local PRODUCTION_COMMIT = "80472cf0eb7243e3fe571b022b6e2a069838d07a"
 local commit = shared.CustomCommit or (shared.TestingMode or shared.StagingMode) and TESTING_COMMIT or PRODUCTION_COMMIT
 shared.META_COMMIT = "a109a0a4441e42d497fa7e3cdc04d770dd853a04"
 if (isfile("vape/profiles/commit.txt") and readfile("vape/profiles/commit.txt") or "") ~= commit then
